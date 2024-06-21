7265fa379836652b6abc8e39437628ffd647ae92
Priyanshu0007 <priyanshu2859@gmail.com>
2024-06-21 15:32:51 +0530

    third commit

--- src/ChatBubble.js
+++ src/ChatBubble.js
@@ -6,7 +6,7 @@ export default function ChatBubble({role, text, onSpeech}) {
     <View className={`mb-2 p-2 max-w-[70%] rounded-2xl relative ${role==="user" ? "self-end bg-blue-100" : "self-start bg-blue-200"}`}>
       <Text className="text-lg">{text}</Text>
       {role === "model" && (<TouchableOpacity className="" onPress={onSpeech}>
-        <Ionicons className="absolute bottom-[5px] right-[5px]" name="volume-high-outline" size={24} color="#fff"/>
+        <Ionicons className="bottom-[5px] right-[5px]" name="volume-high-outline" size={24} color="#fff"/>
       </TouchableOpacity>)}
     </View>
   )
